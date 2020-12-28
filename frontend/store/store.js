f5f5fd5bea8c6b7632adb6d87f7279c62e844402
italo <italo.lujanpedreschi@gmail.com>
2020-12-28 13:19:34 -0800

    Fix redux logger #4

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,22 +3,20 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/root_reducer';
 
-if (process.env.NODE_ENV === 'development') {
-    return createStore(
-        rootReducer,
-        preloadedState,
-        applyMiddleware(thunk, logger)
-    )
-} else if (process.env.NODE_ENV === 'production') {
-    return createStore(
-        rootReducer,
-        preloadedState,
-        applyMiddleware(thunk)
-    )
+const configureStore = (preloadedState = {}) => {
+    if (process.env.NODE_ENV === 'development') {
+        return createStore(
+            rootReducer,
+            preloadedState,
+            applyMiddleware(thunk, logger)
+        )
+    } else if (process.env.NODE_ENV === 'production') {
+        return createStore(
+            rootReducer,
+            preloadedState,
+            applyMiddleware(thunk)
+        )
+    }
 }
-
-const configureStore = (preloadedState = {}) => (
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
-);
 
 export default configureStore;
